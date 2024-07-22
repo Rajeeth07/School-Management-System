3510f801d8aa3930806e58575e1f921e829f8435
Rajeeth 07 <94297225+Rajeeth07@users.noreply.github.com>
2024-07-22 13:39:04 +0530

    Designed Student Master and Create Student Form UI

--- School Management System/UI/Student/StudentEditForm.Designer.cs
+++ School Management System/UI/Student/StudentEditForm.Designer.cs
@@ -28,12 +28,344 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbStdCrtGrdId = new System.Windows.Forms.ComboBox();
+            this.dtpStdCrtAddDate = new System.Windows.Forms.DateTimePicker();
+            this.dtpStdcrtDoB = new System.Windows.Forms.DateTimePicker();
+            this.rdoStdCrtMale = new System.Windows.Forms.RadioButton();
+            this.rdoStdCrtFemale = new System.Windows.Forms.RadioButton();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnStdcrtNic = new System.Windows.Forms.TextBox();
+            this.btnStdCreate = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStdcrtaddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnStdcrtphoneNo = new System.Windows.Forms.TextBox();
+            this.btnStdcrtLname = new System.Windows.Forms.TextBox();
+            this.btnStdcrtfullName = new System.Windows.Forms.TextBox();
+            this.btnStdcrtfname = new System.Windows.Forms.TextBox();
+            this.btnStdcrtAddmisNo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStdcrtCancel = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // cmbStdCrtGrdId
+            // 
+            this.cmbStdCrtGrdId.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbStdCrtGrdId.FormattingEnabled = true;
+            this.cmbStdCrtGrdId.Location = new System.Drawing.Point(172, 361);
+            this.cmbStdCrtGrdId.Name = "cmbStdCrtGrdId";
+            this.cmbStdCrtGrdId.Size = new System.Drawing.Size(272, 33);
+            this.cmbStdCrtGrdId.TabIndex = 6;
+            // 
+            // dtpStdCrtAddDate
+            // 
+            this.dtpStdCrtAddDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStdCrtAddDate.Location = new System.Drawing.Point(172, 404);
+            this.dtpStdCrtAddDate.Name = "dtpStdCrtAddDate";
+            this.dtpStdCrtAddDate.Size = new System.Drawing.Size(272, 30);
+            this.dtpStdCrtAddDate.TabIndex = 5;
+            // 
+            // dtpStdcrtDoB
+            // 
+            this.dtpStdcrtDoB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpStdcrtDoB.Location = new System.Drawing.Point(172, 232);
+            this.dtpStdcrtDoB.Name = "dtpStdcrtDoB";
+            this.dtpStdcrtDoB.Size = new System.Drawing.Size(272, 30);
+            this.dtpStdcrtDoB.TabIndex = 5;
+            // 
+            // rdoStdCrtMale
+            // 
+            this.rdoStdCrtMale.Location = new System.Drawing.Point(172, 189);
+            this.rdoStdCrtMale.Name = "rdoStdCrtMale";
+            this.rdoStdCrtMale.Size = new System.Drawing.Size(84, 35);
+            this.rdoStdCrtMale.TabIndex = 4;
+            this.rdoStdCrtMale.TabStop = true;
+            this.rdoStdCrtMale.Text = "Male";
+            this.rdoStdCrtMale.UseVisualStyleBackColor = true;
+            // 
+            // rdoStdCrtFemale
+            // 
+            this.rdoStdCrtFemale.Location = new System.Drawing.Point(279, 189);
+            this.rdoStdCrtFemale.Name = "rdoStdCrtFemale";
+            this.rdoStdCrtFemale.Size = new System.Drawing.Size(99, 35);
+            this.rdoStdCrtFemale.TabIndex = 4;
+            this.rdoStdCrtFemale.TabStop = true;
+            this.rdoStdCrtFemale.Text = "Female";
+            this.rdoStdCrtFemale.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.Location = new System.Drawing.Point(7, 447);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(152, 35);
+            this.label12.TabIndex = 2;
+            this.label12.Text = "Address";
+            // 
+            // label11
+            // 
+            this.label11.Location = new System.Drawing.Point(7, 404);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(152, 35);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Addimission Date";
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(7, 232);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(152, 35);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "DOB";
+            // 
+            // label10
+            // 
+            this.label10.Location = new System.Drawing.Point(7, 275);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(152, 35);
+            this.label10.TabIndex = 2;
+            this.label10.Text = "NIC Number";
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(7, 361);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(152, 35);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Grade ID";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(7, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(152, 35);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Last Name";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(7, 146);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 35);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Full Name";
+            // 
+            // btnStdcrtNic
+            // 
+            this.btnStdcrtNic.Location = new System.Drawing.Point(172, 271);
+            this.btnStdcrtNic.Multiline = true;
+            this.btnStdcrtNic.Name = "btnStdcrtNic";
+            this.btnStdcrtNic.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtNic.TabIndex = 3;
+            // 
+            // btnStdCreate
+            // 
+            this.btnStdCreate.Location = new System.Drawing.Point(278, 20);
+            this.btnStdCreate.Name = "btnStdCreate";
+            this.btnStdCreate.Size = new System.Drawing.Size(75, 40);
+            this.btnStdCreate.TabIndex = 0;
+            this.btnStdCreate.Text = "Save";
+            this.btnStdCreate.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(14, 189);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(152, 35);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Gender";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cmbStdCrtGrdId);
+            this.groupBox1.Controls.Add(this.dtpStdCrtAddDate);
+            this.groupBox1.Controls.Add(this.dtpStdcrtDoB);
+            this.groupBox1.Controls.Add(this.rdoStdCrtMale);
+            this.groupBox1.Controls.Add(this.rdoStdCrtFemale);
+            this.groupBox1.Controls.Add(this.label12);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.btnStdcrtaddress);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnStdcrtNic);
+            this.groupBox1.Controls.Add(this.btnStdcrtphoneNo);
+            this.groupBox1.Controls.Add(this.btnStdcrtLname);
+            this.groupBox1.Controls.Add(this.btnStdcrtfullName);
+            this.groupBox1.Controls.Add(this.btnStdcrtfname);
+            this.groupBox1.Controls.Add(this.btnStdcrtAddmisNo);
+            this.groupBox1.Location = new System.Drawing.Point(5, 44);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(456, 512);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(7, 318);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(152, 35);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Phone No";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(7, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(152, 35);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First name";
+            // 
+            // btnStdcrtaddress
+            // 
+            this.btnStdcrtaddress.Location = new System.Drawing.Point(172, 441);
+            this.btnStdcrtaddress.Multiline = true;
+            this.btnStdcrtaddress.Name = "btnStdcrtaddress";
+            this.btnStdcrtaddress.Size = new System.Drawing.Size(272, 65);
+            this.btnStdcrtaddress.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(7, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Addmission No";
+            // 
+            // btnStdcrtphoneNo
+            // 
+            this.btnStdcrtphoneNo.Location = new System.Drawing.Point(172, 318);
+            this.btnStdcrtphoneNo.Multiline = true;
+            this.btnStdcrtphoneNo.Name = "btnStdcrtphoneNo";
+            this.btnStdcrtphoneNo.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtphoneNo.TabIndex = 3;
+            // 
+            // btnStdcrtLname
+            // 
+            this.btnStdcrtLname.Location = new System.Drawing.Point(172, 103);
+            this.btnStdcrtLname.Multiline = true;
+            this.btnStdcrtLname.Name = "btnStdcrtLname";
+            this.btnStdcrtLname.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtLname.TabIndex = 3;
+            // 
+            // btnStdcrtfullName
+            // 
+            this.btnStdcrtfullName.Location = new System.Drawing.Point(172, 146);
+            this.btnStdcrtfullName.Multiline = true;
+            this.btnStdcrtfullName.Name = "btnStdcrtfullName";
+            this.btnStdcrtfullName.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtfullName.TabIndex = 3;
+            // 
+            // btnStdcrtfname
+            // 
+            this.btnStdcrtfname.Location = new System.Drawing.Point(172, 60);
+            this.btnStdcrtfname.Multiline = true;
+            this.btnStdcrtfname.Name = "btnStdcrtfname";
+            this.btnStdcrtfname.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtfname.TabIndex = 3;
+            // 
+            // btnStdcrtAddmisNo
+            // 
+            this.btnStdcrtAddmisNo.Location = new System.Drawing.Point(172, 17);
+            this.btnStdcrtAddmisNo.Multiline = true;
+            this.btnStdcrtAddmisNo.Name = "btnStdcrtAddmisNo";
+            this.btnStdcrtAddmisNo.Size = new System.Drawing.Size(272, 35);
+            this.btnStdcrtAddmisNo.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(800, 30);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Student Edit";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStdcrtCancel);
+            this.groupBox2.Controls.Add(this.btnStdCreate);
+            this.groupBox2.Location = new System.Drawing.Point(5, 556);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(456, 61);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnStdcrtCancel
+            // 
+            this.btnStdcrtCancel.Location = new System.Drawing.Point(369, 20);
+            this.btnStdcrtCancel.Name = "btnStdcrtCancel";
+            this.btnStdcrtCancel.Size = new System.Drawing.Size(75, 40);
+            this.btnStdcrtCancel.TabIndex = 0;
+            this.btnStdcrtCancel.Text = "Cancel";
+            this.btnStdcrtCancel.UseVisualStyleBackColor = true;
+            // 
+            // StudentEditForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 628);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "StudentEditForm";
             this.Text = "StudentEditForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbStdCrtGrdId;
+        private System.Windows.Forms.DateTimePicker dtpStdCrtAddDate;
+        private System.Windows.Forms.DateTimePicker dtpStdcrtDoB;
+        private System.Windows.Forms.RadioButton rdoStdCrtMale;
+        private System.Windows.Forms.RadioButton rdoStdCrtFemale;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox btnStdcrtNic;
+        private System.Windows.Forms.Button btnStdCreate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox btnStdcrtaddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox btnStdcrtphoneNo;
+        private System.Windows.Forms.TextBox btnStdcrtLname;
+        private System.Windows.Forms.TextBox btnStdcrtfullName;
+        private System.Windows.Forms.TextBox btnStdcrtfname;
+        private System.Windows.Forms.TextBox btnStdcrtAddmisNo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStdcrtCancel;
     }
 }
--- School Management System/UI/Student/StudentMasterForm.Designer.cs
+++ School Management System/UI/Student/StudentMasterForm.Designer.cs
@@ -28,12 +28,136 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnStdExit = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnStdRefresh = new System.Windows.Forms.Button();
+            this.btnStdDelete = new System.Windows.Forms.Button();
+            this.btnStdAdd = new System.Windows.Forms.Button();
+            this.btnStdEdit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStdExit
+            // 
+            this.btnStdExit.Location = new System.Drawing.Point(768, 19);
+            this.btnStdExit.Name = "btnStdExit";
+            this.btnStdExit.Size = new System.Drawing.Size(98, 48);
+            this.btnStdExit.TabIndex = 0;
+            this.btnStdExit.Text = "Exit";
+            this.btnStdExit.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(873, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Student Master";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(874, 335);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(-1, 39);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(874, 347);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnStdRefresh);
+            this.groupBox2.Controls.Add(this.btnStdDelete);
+            this.groupBox2.Controls.Add(this.btnStdAdd);
+            this.groupBox2.Controls.Add(this.btnStdEdit);
+            this.groupBox2.Controls.Add(this.btnStdExit);
+            this.groupBox2.Location = new System.Drawing.Point(-1, 382);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(874, 75);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // btnStdRefresh
+            // 
+            this.btnStdRefresh.Location = new System.Drawing.Point(352, 19);
+            this.btnStdRefresh.Name = "btnStdRefresh";
+            this.btnStdRefresh.Size = new System.Drawing.Size(98, 48);
+            this.btnStdRefresh.TabIndex = 0;
+            this.btnStdRefresh.Text = "Refresh";
+            this.btnStdRefresh.UseVisualStyleBackColor = true;
+            // 
+            // btnStdDelete
+            // 
+            this.btnStdDelete.Location = new System.Drawing.Point(456, 19);
+            this.btnStdDelete.Name = "btnStdDelete";
+            this.btnStdDelete.Size = new System.Drawing.Size(98, 48);
+            this.btnStdDelete.TabIndex = 0;
+            this.btnStdDelete.Text = "Delete";
+            this.btnStdDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnStdAdd
+            // 
+            this.btnStdAdd.Location = new System.Drawing.Point(560, 19);
+            this.btnStdAdd.Name = "btnStdAdd";
+            this.btnStdAdd.Size = new System.Drawing.Size(98, 48);
+            this.btnStdAdd.TabIndex = 0;
+            this.btnStdAdd.Text = "Add";
+            this.btnStdAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnStdEdit
+            // 
+            this.btnStdEdit.Location = new System.Drawing.Point(664, 19);
+            this.btnStdEdit.Name = "btnStdEdit";
+            this.btnStdEdit.Size = new System.Drawing.Size(98, 48);
+            this.btnStdEdit.TabIndex = 0;
+            this.btnStdEdit.Text = "Edit";
+            this.btnStdEdit.UseVisualStyleBackColor = true;
+            // 
+            // StudentMasterForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(873, 461);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "StudentMasterForm";
             this.Text = "StudentMasterForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStdExit;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnStdRefresh;
+        private System.Windows.Forms.Button btnStdDelete;
+        private System.Windows.Forms.Button btnStdAdd;
+        private System.Windows.Forms.Button btnStdEdit;
     }
 }
